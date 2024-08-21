a3e847ab176ce65407594e57e7539ea0e605603e
Murillo Oliveira <murillo1047@gmail.com>
2024-08-21 16:35:51 -0300

    feat: add database context and configuration classes

---
+++ Context/Mappings/MedicoMap.cs
@@ -0,0 +1,38 @@
+﻿using APIThatSendAnEmailToUpdatePassword.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIThatSendAnEmailToUpdatePassword.Context.Mappings
+{
+    public class MedicoMap : IEntityTypeConfiguration<Medico>
+    {
+        public void Configure(EntityTypeBuilder<Medico> builder)
+        {
+            builder.ToTable("medicos")
+            .HasKey(m => m.Id);
+
+            builder.Property(m => m.Especialidade)
+            .HasColumnType("VARCHAR");
+
+            builder.Property(m => m.CRM)
+            .HasColumnType("VARCHAR")
+            .IsRequired();
+
+            builder.Property(m => m.DataCriacao)
+            .HasColumnType("timestamp")
+            .IsRequired();
+
+            builder.Property(m => m.DataInativacao)
+            .HasColumnType("timestamp");
+
+            builder.Property(m => m.UserId)
+                .HasColumnType("VARCHAR")
+                .IsRequired();
+
+            builder.HasOne(m => m.Usuario)
+               .WithOne()
+               .HasForeignKey<Medico>(m => m.UserId) // Define o campo UserId como chave estrangeira
+               .OnDelete(DeleteBehavior.Cascade); // Define o comportamento de exclusão em cascata
+        }
+    }
+}
---
+++ Context/UsersContext.cs
@@ -0,0 +1,21 @@
+﻿using APIThatSendAnEmailToUpdatePassword.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIThatSendAnEmailToUpdatePassword.Context
+{
+    public class UsersContext : IdentityDbContext<Usuario>
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Medico> Medicos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
