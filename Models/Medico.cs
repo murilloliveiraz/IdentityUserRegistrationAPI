503410f1fd2ff42a3cf06aa7e523aa6336c39f77
Murillo Oliveira <murillo1047@gmail.com>
2024-08-21 16:36:16 -0300

    feat: add model classes representing the domain entities

---
+++ Models/Medico.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace APIThatSendAnEmailToUpdatePassword.Models
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string Especialidade { get; set; }
+        public string CRM { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataInativacao { get; set; }
+        public string UserId { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace APIThatSendAnEmailToUpdatePassword.Models
+{
+    public class Usuario : IdentityUser
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Role {  get; set; }
+    }
+}
