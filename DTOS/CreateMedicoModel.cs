140d8fa0da474bc3d16194cb8c233df37472ea9f
Murillo Oliveira <murillo1047@gmail.com>
2024-08-21 16:35:58 -0300

    feat: add Data Transfer Objects (DTOs) for input and output handling

---
+++ DTOS/CreateMedicoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace APIThatSendAnEmailToUpdatePassword.DTOS
+{
+    public class CreateMedicoModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Especialidade { get; set; }
+        public string FrontendUrl { get; set; }
+        public string CRM { get; set; }
+        public string CPF { get; set; }
+    }
+}
